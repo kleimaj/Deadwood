bf91f810b96cef825840648bddeb5168074d4d05
kleimaj <kleimaj@wwu.edu>
2017-11-15 13:57:32 -0800

    Parser done?:

--- Deadwood.java
+++ Deadwood.java
@@ -47,7 +47,7 @@ public class Deadwood {
 		//Location[] allLocations = new Location[0];
 		
 	//begin comment here
-		Board game = new Board(allLocations, deck);
+/*		Board game = new Board(allLocations, deck);
 		for (int i = 0; i < numPlayers; i++) {
 			 String name = console.next();
 			 game.addPlayer(new Player(name, trailer));
@@ -65,6 +65,11 @@ public class Deadwood {
 				}
 					Player currentPlayer = players[index];
 					System.out.println(currentPlayer.getName()+"'s turn!");
+					System.out.println("Player: "+currentPlayer.getName()+" ($"+currentPlayer.getCurrency()+", "+currentPlayer.getFame()+" Fame)");
+					if (currentPlayer.getRole() != null) {
+						System.out.println("Working as "+currentPlayer.getRole().getName()+", '"+currentPlayer.getRole().getDialogue()+"'");
+					}
+					System.out.println(currentPlayer.name+" is currently in "+currentPlayer.getLocation().getName());
 					
 						//Start loop 1
 				while(true) {
@@ -133,7 +138,7 @@ public class Deadwood {
 					}
 					if (action.equals("upgrade")) {
 						currentPlayer.Upgrade();//effect endTurn = ...    //can player upgrade then move? if yes, can they take a role after?
-						break;
+						//break;
 					}
 					if (action.equals("act")) {
 						currentPlayer.Act();//effect endTurn = ...
@@ -178,11 +183,11 @@ public class Deadwood {
 		String playAgain = console.nextLine();
 		
 		if (playAgain.equals("n")) {
-			//break
+			//breaklocations
 		}
 		
 			
-		console.close();	 //end comment here			
+		console.close();	 //end comment here	*/		
 	} 
 	
 	
@@ -364,7 +369,7 @@ public class Deadwood {
 						parts = new Role[roles.size()];
 				    	parts = roles.toArray(parts);
 	    			}
-	    	/*		System.out.println("SET: " + set_name + " " + takeNum + " ");
+	    		/*	System.out.println("SET: " + set_name + " " + takeNum + " ");
 	    			System.out.println("ROLES:");
 	    			for (int z = 0; z < parts.length; z++) {
 	    				System.out.println(parts[z].getName());
@@ -372,14 +377,14 @@ public class Deadwood {
 	    			System.out.println("NEIGHBORS:");
 	    			for (int w = 0; w < _neighbors.length; w++) {
 	    				System.out.println(_neighbors[w]);
-	    			} */
+	    			}   */
 	    			Location newLocation = new Location(set_name, takeNum, 0, null, parts);
 	    			newLocation.setNeighbors(_neighbors);
 	    			locations.add(newLocation);
 	    		}
 	    	}
 	    	// TRAILER ----------------------------------------------------------------->
-	 /*   	NodeList trailer = doc.getElementsByTagName("trailer");
+	    	NodeList trailer = doc.getElementsByTagName("trailer");
 	    	Node trailerNode = trailer.item(0);
 	    	ArrayList<String> neighbors = new ArrayList<String>();
 	    	_neighbors = null;
@@ -389,28 +394,38 @@ public class Deadwood {
 	    		Node _nNode = neighborList.item(temp); 
 	    		if (_nNode.getNodeType() == Node.ELEMENT_NODE) {
 					Element nElement = (Element) _nNode;
-					neighbor = nElement.getAttribute("name");
-					neighbors.add(neighbor);
+					switch (_nNode.getNodeName()) {
+					case "neighbors":
+						NodeList dList = _nNode.getChildNodes();
+						for (int n = 0; n < dList.getLength(); n++) {
+							Node eNode = dList.item(n);
+							if (eNode.getNodeType() == Node.ELEMENT_NODE) {
+		    					Element fElement = (Element) eNode;
+		    					neighbor = fElement.getAttribute("name");
+		    					neighbors.add(neighbor);
+							}
+						}
+					}
+	    			
 				}
 			}
 	    	_neighbors = new String[neighbors.size()];
 			_neighbors = neighbors.toArray(_neighbors);
-	    	
+
 	    	Location newLocation = new Location("Trailer");
 	    	newLocation.setNeighbors(_neighbors);
 	    	locations.add(newLocation);
 	    	
 	    	// CASTING OFFICE ---------------------------------------------------------->
 	    	NodeList office = doc.getElementsByTagName("office");
-	    	Node officeNode = trailer.item(0);
-	    	ArrayList<String> neighbors = new ArrayList<String>();
+	    	Node officeNode = office.item(0);
+	    	neighbors = new ArrayList<String>();
 	    	_neighbors = null;
 	    	
 	    	NodeList officeList = officeNode.getChildNodes();
 	    	for (int temp = 0; temp < officeList.getLength(); temp++) {
 	    		Node oNode = officeList.item(temp);
 	    		if (oNode.getNodeType() == Node.ELEMENT_NODE) {
-	    			Element office_e = (Element) office;
 	    			switch (oNode.getNodeName()){
 	    			case "neighbors":
 	    				NodeList nList2 = oNode.getChildNodes();
@@ -426,15 +441,309 @@ public class Deadwood {
 						_neighbors = neighbors.toArray(_neighbors);
 		    			break;
 	    			case "upgrades":
+	    				// Create GUI objects here
 	    				break;
 	    			}
 	    		}
-	    	} */
+	    	}
+	    	
+	    	_neighbors = new String[neighbors.size()];
+			_neighbors = neighbors.toArray(_neighbors);
+
+			newLocation = new Location("Office");
+	    	newLocation.setNeighbors(_neighbors);
+	    	locations.add(newLocation);
+	    	
 	    } 
 	    
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+	
+	public Deck getDeck(File input) {
+		try {
+			
+			// Create a document builder
+			DocumentBuilderFactory dbFactory  = DocumentBuilderFactory.newInstance();
+		    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+		    
+		    // Create a document from a file
+		    Document doc = dBuilder.parse(input);   
+		    doc.getDocumentElement().normalize();
+		    
+		    // Extract the root and other elements
+		    Element root = doc.getDocumentElement();
+		    
+			    NodeList cardList = doc.getElementsByTagName("card"); 
+			    
+			    int budget = 0;
+			    int scene_num = 0;
+			    String name = "";
+			    String scene_desc = "";
+			    String p_name;
+			    String p_line = "";
+			    int p_lvl;
+			    LinkedList<Scene> scenes = new LinkedList<Scene>();
+			    
+			    for (int i = 0; i < cardList.getLength(); i++) {
+			    	
+			    	Node cardNode = cardList.item(i); 
+			    	ArrayList<Role> roles = new ArrayList<Role>();
+			    	
+			    	if (cardNode.getNodeType() == Node.ELEMENT_NODE) {		
+			    		Element cardElement = (Element) cardNode;
+			    		name = cardElement.getAttribute("name");
+			    		budget = Integer.parseInt(cardElement.getAttribute("budget"));
+				    	NodeList pList = cardNode.getChildNodes(); 
+				    	
+				    	for (int p = 0; p < pList.getLength(); p++) {
+				    		
+				    		Node pNode = pList.item(p);
+				    		if (pNode.getNodeType() == Node.ELEMENT_NODE) {
+				    			Element pElement = (Element) pNode;
+					    		switch (pNode.getNodeName()) {
+					    		case "scene": 
+					    			scene_num = Integer.parseInt(pElement.getAttribute("number"));
+					    			scene_desc = cardElement.getElementsByTagName("scene").item(0).getTextContent();
+					    			break;
+					    		case "part":
+					    			NodeList pList2 = pNode.getChildNodes();
+					    			p_name = pElement.getAttribute("name");
+					    			p_lvl = Integer.parseInt(pElement.getAttribute("level"));
+					    			for (int q = 0; q < pList2.getLength(); q++) {
+					    				Node qNode = pList2.item(q);		
+					    				if (qNode.getNodeType() == Node.ELEMENT_NODE) {
+					    					Element qElement = (Element) qNode;
+					    					switch (qNode.getNodeName()) {
+					    					case "line":
+					    						p_line = pElement.getElementsByTagName("line").item(0).getTextContent();
+					    						break;
+					    					case "area":
+					    						break;
+					    					}
+					    				}
+					    			}
+					    			//System.out.println(p_name + "-" + p_line + "-" + p_lvl);
+					    			Role newRole = new Role(p_name, p_line, p_lvl, true);
+					    			roles.add(newRole);
+					    		}	
+				    		}
+				    	}
+			    	}
+			    	Role[] parts = new Role[roles.size()];
+			    	parts = roles.toArray(parts);
+			    	Scene newScene = new Scene(name, scene_desc, budget, scene_num, parts);
+			    	scenes.add(newScene);
+			    	
+			    
+			    }
+			    Deck deck = new Deck(scenes);
+				return deck;
+			    }
+		catch (Exception e) {
+				e.printStackTrace();
+			}
+		return null;
+	}
+	
+	public Location[] getLocations(File input) {
+		
+		Location[] allLocations = null;
+		try {
+			
+		// Create a document builder
+		DocumentBuilderFactory dbFactory  = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	    
+	    // Create a document from a file
+	    Document doc = dBuilder.parse(input);   
+	    doc.getDocumentElement().normalize();
+	    
+	    // Extract the root and other elements
+	    //Element root = doc.getDocumentElement();
+		// root is board
+    	// loop through sets first
+    	
+    	ArrayList<Location> locations = new ArrayList<Location>();
+    	
+    	NodeList setList = doc.getElementsByTagName("set");
+    	
+    	String set_name;
+    	String neighbor;
+    	int takeNum = 0; // shot max
+    	Role[] parts = null;
+    	String[] _neighbors = null;
+		int p_lvl = 0;
+		String p_name = "";
+		String p_line = "";
+		
+    	for (int i = 0; i < setList.getLength(); i++) {
+    		takeNum = 0;
+    		parts = null;
+    		ArrayList<Role> roles = new ArrayList<Role>();
+	    	ArrayList<String> neighbors = new ArrayList<String>();
+	    	
+    		Node setNode = setList.item(i);
+    		if (setNode.getNodeType() == Node.ELEMENT_NODE) {
+    			Element setElement = (Element) setNode;
+    			set_name = setElement.getAttribute("name");
+    			NodeList bigList = setNode.getChildNodes();
+    			
+    			for (int j = 0; j < bigList.getLength(); j++) {
+    				Node tNode = bigList.item(j);
+    				if (tNode.getNodeType() == Node.ELEMENT_NODE) {
+    					//Element tElement = (Element) tNode;
+    					switch (tNode.getNodeName()) {
+    					case "neighbors":
+    						NodeList nList = tNode.getChildNodes();
+    						for (int n = 0; n < nList.getLength(); n++) {
+    							Node nNode = nList.item(n);
+    							if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+			    					Element nElement = (Element) nNode;
+			    					neighbor = nElement.getAttribute("name");
+			    					neighbors.add(neighbor);
+    							}
+    						}
+    						_neighbors = new String[neighbors.size()];
+    						_neighbors = neighbors.toArray(_neighbors);
+    						break;
+    					case "takes":
+    						NodeList takeList = tNode.getChildNodes();
+    						for (int k = 0; k < takeList.getLength(); k++) {
+    							// create take objects here (GUI)
+    							Node takeNode = takeList.item(k);
+    							if (takeNode.getNodeType() == Node.ELEMENT_NODE) {
+	    							takeNum++;
+    							}
+    						}
+    						break;
+    						
+    					case "parts":
+    						NodeList partsList = tNode.getChildNodes();
+    						for (int p = 0; p < partsList.getLength(); p++) {
+    							Node partNode = partsList.item(p);
+    							if (partNode.getNodeType() == Node.ELEMENT_NODE) {
+    								Element pElement = (Element) partNode;
+    								p_name = pElement.getAttribute("name");
+    								p_lvl = Integer.parseInt(pElement.getAttribute("level"));
+    								NodeList partList2 = partNode.getChildNodes();	
+    								for (int q = 0; q < partList2.getLength(); q++) {
+    									Node qNode = partList2.item(q);		
+    				    				if (qNode.getNodeType() == Node.ELEMENT_NODE) {
+    				    					//Element qElement = (Element) qNode;
+    				    					switch (qNode.getNodeName()) {
+    				    					case "line":
+    				    						p_line = pElement.getElementsByTagName("line").item(0).getTextContent();
+    				    						break;
+    				    					case "area":
+    				    						break;
+    				    					}
+    				    				}
+    								}
+        							//System.out.println("Role is: " + p_name + ". Level is: " + p_lvl + ". Line is: " + p_line);
+	    							Role newRole = new Role(p_name, p_line, p_lvl, false);
+	    							roles.add(newRole);
+    							}
+    						}
+    					}
+    				}
+					parts = new Role[roles.size()];
+			    	parts = roles.toArray(parts);
+    			}
+    		/*	System.out.println("SET: " + set_name + " " + takeNum + " ");
+    			System.out.println("ROLES:");
+    			for (int z = 0; z < parts.length; z++) {
+    				System.out.println(parts[z].getName());
+    			}
+    			System.out.println("NEIGHBORS:");
+    			for (int w = 0; w < _neighbors.length; w++) {
+    				System.out.println(_neighbors[w]);
+    			}   */
+    			Location newLocation = new Location(set_name, takeNum, 0, null, parts);
+    			newLocation.setNeighbors(_neighbors);
+    			locations.add(newLocation);
+    		}
+    	}
+    	// TRAILER ----------------------------------------------------------------->
+    	NodeList trailer = doc.getElementsByTagName("trailer");
+    	Node trailerNode = trailer.item(0);
+    	ArrayList<String> neighbors = new ArrayList<String>();
+    	_neighbors = null;
+    	
+    	NodeList neighborList = trailerNode.getChildNodes();
+    	for (int temp = 0; temp < neighborList.getLength(); temp++) {
+    		Node _nNode = neighborList.item(temp); 
+    		if (_nNode.getNodeType() == Node.ELEMENT_NODE) {
+				//Element nElement = (Element) _nNode;
+				switch (_nNode.getNodeName()) {
+				case "neighbors":
+					NodeList dList = _nNode.getChildNodes();
+					for (int n = 0; n < dList.getLength(); n++) {
+						Node eNode = dList.item(n);
+						if (eNode.getNodeType() == Node.ELEMENT_NODE) {
+	    					Element fElement = (Element) eNode;
+	    					neighbor = fElement.getAttribute("name");
+	    					neighbors.add(neighbor);
+						}
+					}
+				}
+    			
+			}
+		}
+    	_neighbors = new String[neighbors.size()];
+		_neighbors = neighbors.toArray(_neighbors);
+
+    	Location newLocation = new Location("Trailer");
+    	newLocation.setNeighbors(_neighbors);
+    	locations.add(newLocation);
+    	
+    	// CASTING OFFICE ---------------------------------------------------------->
+    	NodeList office = doc.getElementsByTagName("office");
+    	Node officeNode = office.item(0);
+    	neighbors = new ArrayList<String>();
+    	_neighbors = null;
+    	
+    	NodeList officeList = officeNode.getChildNodes();
+    	for (int temp = 0; temp < officeList.getLength(); temp++) {
+    		Node oNode = officeList.item(temp);
+    		if (oNode.getNodeType() == Node.ELEMENT_NODE) {
+    			switch (oNode.getNodeName()){
+    			case "neighbors":
+    				NodeList nList2 = oNode.getChildNodes();
+					for (int n = 0; n < nList2.getLength(); n++) {
+						Node n_Node = nList2.item(n);
+						if (n_Node.getNodeType() == Node.ELEMENT_NODE) {
+	    					Element n_Element = (Element) n_Node;
+	    					neighbor = n_Element.getAttribute("name");
+	    					neighbors.add(neighbor);
+						}
+					}
+					_neighbors = new String[neighbors.size()];
+					_neighbors = neighbors.toArray(_neighbors);
+	    			break;
+    			case "upgrades":
+    				// Create GUI objects here
+    				break;
+    			}
+    		}
+    	}
+    	
+    	_neighbors = new String[neighbors.size()];
+		_neighbors = neighbors.toArray(_neighbors);
+
+		newLocation = new Location("Office");
+    	newLocation.setNeighbors(_neighbors);
+    	locations.add(newLocation);
+    	allLocations = locations.toArray(allLocations);
+    	
+    	
+   
+    
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+		return allLocations;
 	}
 }
